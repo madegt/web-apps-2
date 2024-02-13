abdd935b4248943d1aba8b19265b45a612baf984
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-02-13 16:57:43 +0000

    list

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,14 +1,30 @@
 class CompaniesController < ApplicationController
 
   def index
+
+    @companies = Company.all
+
     # find all Company rows
     # render companies/index view
   end
 
+  
+#show request to go into the company profile
   # def show
   #   # find a Company
   #   # render companies/show view with details about Company
   # end
+
+def show
+#it renders under views>show > no show for each, is one for all with same strucutre for everyone
+#static
+#company=Company.find_by({"id"=>1})
+
+#using params
+@company=Company.find_by({"id"=>params["id"]}) 
+
+end
+
 
   # def new
   #   # render view with new Company form
